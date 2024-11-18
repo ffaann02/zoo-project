570b89ffe15cf6cdbb2d9b6265b956a5958ad8ce
Rudfaan Maimahad <84792604+ffaann02@users.noreply.github.com>
2024-11-18 07:14:57 +0000

    add: age test cases and conditions

--- test_zoo.py
+++ test_zoo.py
@@ -8,23 +8,23 @@ class TestZoo(unittest.TestCase):
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
 
-    def test_block_C1b1(self):
+    def test_negative_age(self):
         # C1b1: age = -1, ticket price = 0
         self.assertEqual(self.zoo.get_ticket_price(-1), 0)
 
-    def test_block_C1b2(self):
+    def test_child_age(self):
         # C1b2: age = 12, ticket price = 50
         self.assertEqual(self.zoo.get_ticket_price(12), 50)
 
-    def test_block_C1b3(self):
+    def test_teen_age(self):
         # C1b3: age = 13, ticket price = 100
-        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
 
-    def test_block_C1b4(self):
+    def test_adult_age(self):
         # C1b4: age = 60, ticket price = 150
         self.assertEqual(self.zoo.get_ticket_price(60), 150)
 
-    def test_block_C1b5(self):
+    def test_elder_age(self):
         # C1b5: age = 61, ticket price = 100
         self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
--- zoo.py
+++ zoo.py
@@ -1,12 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: # add equal underbound
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: # add equal upperbound
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: # add equal underbound
             return 150
-        elif age >= 60:
+        elif age > 60: # remove equal
             return 100
         else:
             return 0 # add below zero age case
